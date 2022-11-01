ddd959975a6287aa04452c1cbf736dd2279f24b4
Ion Scerbatiuc <ion@rover.com>
2022-11-01 11:12:24 -0700

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 git submodule init .vim/
 git submodule update --init --recursive .vim/
 
 git submodule init .oh-my-zsh/
 git submodule update --init --recursive .oh-my-zsh/
+
+for dotfile in $(ls -1Ap | grep -v "\.git" | grep "^\.[^\.]*$")
+do
+    ln -s "$(pwd)"/"${dotfile}" ~/t/"${dotfile//\//}"
+done
