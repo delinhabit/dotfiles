9639009a55884220efb2ea7d14218000a235b478
Ion Scerbatiuc <ion@rover.com>
2022-11-03 11:02:42 -0700

    Fix symlink creation

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 git submodule init .vim/
 git submodule update --init --recursive .vim/
+ln -sFf "$(pwd)"/.vim "$HOME"/
 
 git submodule init .oh-my-zsh/
 git submodule update --init --recursive .oh-my-zsh/
+ln -sFf "$(pwd)"/.oh-my-zsh "$HOME"/
 
 for dotfile in $(ls -1Ap | grep -v "\.git" | grep "^\.[^\.]*$")
 do
-    ln -sF "$(pwd)"/"${dotfile}" "$HOME"/"${dotfile//\//}"
+    ln -sF "$(pwd)"/"${dotfile}" "$HOME"/
 done
